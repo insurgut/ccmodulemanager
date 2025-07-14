d394e9178dd108877aae0416e65fce0c72b2424a
insurgut <kd4500mmr@gmail.com>
2025-07-14 20:59:27 +0300

    Create temp_reply.module

--- temp_date.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-def cmd_time(account, params):
-    """
-    Динамическая команда, которая возвращает текущее время.
-    Использование: .time
-    """
-    current_time = datetime.datetime.now().strftime("%H:%M:%S")
-    return f"Текущее время: {current_time}"
